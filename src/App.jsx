a1ee4f7cc64a4df67324b100d265e3af79e2bf00
tinykofi <kofiasare163@gmail.com>
2025-10-29 17:57:31 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Navbar,
+  Hero,
+  StarsCanvas,
+  Tech,
+  Feedbacks,
+  Experience,
+  Works,
+  Contact,
+} from "./components";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
